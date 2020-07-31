5d8e77425f517e3eb2a85fb74871a794ff9d4078
Corey Lehman <coreylehman@me.com>
2020-07-31 14:56:56 -0400

    Improvements to ground and air physics.

--- character.py
+++ character.py
@@ -9,21 +9,25 @@ def apply_friction(velocity, friction):
     return velocity - sign(velocity) * min(abs(velocity), friction)
 
 
-def apply_acceleration(velocity, axis_value, base_acceleration, axis_acceleration, max_velocity):
+def apply_acceleration(velocity, axis_value, base_acceleration, axis_acceleration, max_velocity, friction):
+    base_velocity = velocity
+    if abs(base_velocity) > max_velocity:
+        base_velocity = apply_friction(base_velocity, friction)
+
     base = sign(axis_value) * base_acceleration
     axis_addition = axis_value * axis_acceleration
     additional_velocity = base + axis_addition
 
     if axis_value > 0.0:
-        additional_velocity = min(additional_velocity, max_velocity - velocity)
+        additional_velocity = min(additional_velocity, max_velocity - base_velocity)
         additional_velocity = max(0.0, additional_velocity)
     elif axis_value < 0.0:
-        additional_velocity = max(additional_velocity, -max_velocity - velocity)
+        additional_velocity = max(additional_velocity, -max_velocity - base_velocity)
         additional_velocity = min(0.0, additional_velocity)
     else:
         additional_velocity = 0.0
 
-    return velocity + additional_velocity
+    return base_velocity + additional_velocity
 
 
 class Character(object):
@@ -33,13 +37,22 @@ class Character(object):
         self.width = 8.0
         self.height = 16.0
 
-        self.run_velocity = 2.2
-        self.walk_velocity = 1.6
         self.ground_friction = 0.08
+        self.dash_start_velocity = 1.9
+        self.dash_max_velocity = 2.2
+        self.dash_base_acceleration = 0.02
+        self.dash_axis_acceleration = 0.1
+
+        self.walk_start_velocity = 0.16
+        self.walk_max_velocity = 1.6
+        self.walk_base_acceleration = 0.2
+        self.walk_axis_acceleration = 0.0
+
         self.air_friction = 0.02
         self.air_base_acceleration = 0.02
-        self.air_additional_acceleration = 0.06
+        self.air_axis_acceleration = 0.06
         self.air_max_velocity = 0.83
+
         self.jump_squat_frames = 3
         self.short_hop_velocity = 2.1
         self.full_hop_velocity = 3.68
@@ -111,10 +124,30 @@ class Character(object):
             self.x_velocity = apply_friction(self.x_velocity, self.ground_friction)
 
         elif self.state == "walk":
-            self.x_velocity = controller.x_axis.value * self.walk_velocity
+            if abs(self.x_velocity) < self.walk_start_velocity:
+                self.x_velocity = sign(controller.x_axis.value) * self.walk_start_velocity
+
+            self.x_velocity = apply_acceleration(
+                self.x_velocity,
+                controller.x_axis.value,
+                self.walk_base_acceleration,
+                self.walk_axis_acceleration,
+                self.walk_max_velocity,
+                self.ground_friction,
+            )
 
         elif self.state == "run":
-            self.x_velocity = controller.x_axis.value * self.run_velocity
+            if abs(self.x_velocity) < self.dash_start_velocity:
+                self.x_velocity = sign(controller.x_axis.value) * self.dash_start_velocity
+
+            self.x_velocity = apply_acceleration(
+                self.x_velocity,
+                controller.x_axis.value,
+                self.dash_base_acceleration,
+                self.dash_axis_acceleration,
+                self.dash_max_velocity,
+                self.ground_friction,
+            )
 
         elif self.state == "jump_squat":
             self.x_velocity = apply_friction(self.x_velocity, self.ground_friction)
@@ -128,8 +161,9 @@ class Character(object):
                     self.x_velocity,
                     controller.x_axis.value,
                     self.air_base_acceleration,
-                    self.air_additional_acceleration,
+                    self.air_axis_acceleration,
                     self.air_max_velocity,
+                    self.air_friction,
                 )
             else:
                 self.x_velocity = apply_friction(self.x_velocity, self.air_friction)
